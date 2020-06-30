b538999329e25c27d087ec102eb689c9af28ca7c
Ben Thomson <git@alfreido.com>
2020-06-30 16:02:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# WordPress Composer bootstrapper
+
+WordPress is hot garbage - no two ways about it - but given that sometimes we need to shake hands with the devil,
+this repo allows one to bootstrap an installation of WordPress entirely in Composer, and use modern configuration 
+methods.
+
+## Configuration
+
+The configuration is entirely controlled through environment variables, chiefly provided by an `.env` file (although
+you may use actual environment variables if you wish). This file will be created for you automatically if you use 
+`composer create-project`. Otherwise, simply copy `.env.example` to `.env` and away you go.
+
+## Structure
+
+The main structure of a bootstrapped site will be as follows:
+
+- **content/:** contains the plugins and themes folder, along with any other plague-infested folders created by horribly written plugins.
+- **wordpress/:** contains the normal files for a WordPress install.
+- **index.php:** the entrypoint for public-facing pages.
+- **cli:** a small utility program that sets up the encryption keys. It will also be able to install WP automatically at a future point. This is NOT the *WP-CLI* program.
+- **.htaccess:** set up with the basic rewrite rules for WordPress.
+- **index.php:** simple endpoint to load the site.
+- **wp-config.php:** the heavily-modified config file that loads the configuration from the environment. You do not need to edit this file at all. Plugins probably will, but hey, that's the price we pay.
+
+## Installing
+
+Once Composer has done its thing, and you've set up the configuration as required, and you've had enough of your
+favourite alcoholic beverage to prepare yourself, you can install the database tables by going to the following:
+
+```
+https://<WP_SITEURL>/wordpress
+```
+
+This should start up the installer.
+
+## License 
+
+MIT License. No warranties, or takebacksies, provided.
---
+++ wp-config.php
@@ -0,0 +1,66 @@
+<?php
+/** Enable W3 Total Cache */
+define('WP_CACHE', true); // Added by W3 Total Cache
+
+// Load Composer's autoloader
+require_once __DIR__ . '/content/vendor/autoload.php';
+
+// Load configuration
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Path config
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/wordpress/');
+}
+define('WP_HOME', $_ENV['WP_SITEURL']);
+define('WP_SITEURL', $_ENV['WP_SITEURL'] . '/wordpress');
+define('WP_CONTENT_DIR', __DIR__ . '/content');
+define('WP_CONTENT_URL', $_ENV['WP_HOME'] . '/content');
+define('WP_PLUGIN_DIR', __DIR__ . '/content/plugins');
+define('WP_PLUGIN_URL', $_ENV['WP_HOME'] . '/content/plugins');
+
+// Database config
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_CHARSET', $_ENV['DB_CHARSET']);
+define('DB_COLLATE', $_ENV['DB_COLLATE']);
+
+// Encryption key config
+define('AUTH_KEY', $_ENV['AUTH_KEY']);
+define('SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY']);
+define('NONCE_KEY', $_ENV['NONCE_KEY']);
+define('AUTH_SALT', $_ENV['AUTH_SALT']);
+define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT']);
+define('NONCE_SALT', $_ENV['NONCE_SALT']);
+
+// Site configuration
+define('WP_DEBUG', $_ENV['WP_DEBUG']);
+define('WP_POST_REVISIONS', $_ENV['WP_POST_REVISIONS']);
+define('AUTOSAVE_INTERVAL', $_ENV['AUTOSAVE_INTERVAL']);
+define('WP_ALLOW_MULTISITE', $_ENV['WP_ALLOW_MULTISITE']);
+
+// Advanced configuration
+define('FORCE_SSL_ADMIN', $_ENV['FORCE_SSL_ADMIN']);
+define('SCRIPT_DEBUG', $_ENV['SCRIPT_DEBUG']);
+define('CONCATENATE_SCRIPTS', $_ENV['CONCATENATE_SCRIPTS']);
+define('WP_MEMORY_LIMIT', $_ENV['WP_MEMORY_LIMIT']);
+define('WP_MAX_MEMORY_LIMIT', $_ENV['WP_MAX_MEMORY_LIMIT']);
+define('DISABLE_WP_CRON', $_ENV['DISABLE_WP_CRON']);
+define('EMPTY_TRASH_DAYS', $_ENV['EMPTY_TRASH_DAYS']);
+define('IMAGE_EDIT_OVERWRITE', $_ENV['IMAGE_EDIT_OVERWRITE']);
+
+// ----- The following constants are forced, and cannot be overriden by the .env configuration
+
+// Disable automatic updates, as we're using Composer
+define('AUTOMATIC_UPDATER_DISABLED', true);
+define('WP_AUTO_UPDATE_CORE', false);
+
+$table_prefix = $_ENV['TABLE_PREFIX'] ?? 'wp_';
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
